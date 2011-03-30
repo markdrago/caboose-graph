c8684f4b6509bf0e9af271790f682b4439fbbab2
Mark Drago <mdrago@yodle.com>
2011-03-30 18:41:46 -0400

    very early, only sample tests and other junk in place

---
+++ src/render_graph.js
@@ -0,0 +1,4 @@
+function get_data_series_from_results_object(results, dataname) {
+  return [];
+}
+
---
+++ test/tryit.js
@@ -0,0 +1,8 @@
+add_test(function() {
+    assert(false, "Test that fails");
+});
+
+add_test(function() {
+    assert(true, "Test that passes");
+});
+
